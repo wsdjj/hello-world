8dd8f7a3046b659ac4fe7012e1d48faa4524c782
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-04-27 08:43:17 +0800

    Create jddj_cookie.js

---
+++ jddj_cookie.js
@@ -0,0 +1,7 @@
+//nodejs专用,手机用boxjs填写
+//cookie只要里面的deviceid_pdj_jd=xxx-xxx-xxx;o2o_m_h5_sid=xxx-xxx-xxx关键信息
+let cookies = [
+  'deviceid_pdj_jd=H5_DEV_9AAAF588-9411-4186-ABF7-DA6B45F35D5F; o2o_m_h5_sid=ed3b846c-ccc6-46f7-b39d-f839a3e0b3c1',
+  'ck2'
+];
+module.exports = cookies;
